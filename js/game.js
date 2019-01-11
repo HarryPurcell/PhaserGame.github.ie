f72d3c313640cc5302385a916475ec5570e57f0b
HarryPurcell <HarryPurcell@users.noreply.github.com>
2019-01-11 11:05:48 +0000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -29,7 +29,7 @@ gameScene.create = function () {
     
     let bg = this.add.sprite(0, 0, 'background')
     bg.setOrigin(0.0);
-    let endscreen = this.add.sprite(0,0,'endscreen').setOrigin(this.endX,this.endY);
+    let endscreen = this.add.sprite(this.endX,this.endY,'endscreen').setOrigin(0,0);
     this.endscreen.depth = 2;
     
     
